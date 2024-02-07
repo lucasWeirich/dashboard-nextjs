607cb48a85bb00fa13a91e3e77068ed87e304fe4
lucasWeirich <lucasweirich11@gmail.com>
2024-02-07 10:01:38 -0300

    feat: finalized

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,5 +1,110 @@
+"use client"
+
+import Button from "@/components/Button";
+import HeaderTitle from "@/components/HeaderTitle";
+import Paginator from "@/components/Paginator";
+import { api } from "@/lib/api";
+import Cookies from 'js-cookie'
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import LoadingDefault from "../loadings/loadingDefault";
+import { useFormatMoney } from "@/hooks/useFormatMoney";
+import { getUser } from "@/lib/auth";
+import Card from "@/components/Card";
+import ChartAllSales from "@/components/ChartAllSales";
+
+interface ReportProps {
+  orders: number
+  totalSales: number
+  allSales: number
+}
+
 export default function Report() {
-  return <div>
-    Report
-  </div>
+  const [isLoading, setIsLoading] = useState(false)
+  const [report, setReport] = useState<ReportProps>({ orders: 0, totalSales: 0, allSales: 0 })
+
+  const company = getUser()
+  const router = useRouter();
+  const formatedMoney = useFormatMoney;
+
+  async function getReport() {
+    setIsLoading(true);
+    const token = Cookies.get('token')
+
+    try {
+      const report = await api.get('/report', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      setReport(report.data)
+    } catch (err) {
+      // @ts-ignore
+      if (err.response.status === 401) {
+        Cookies.remove('token');
+        router.push('/login');
+      }
+      toast.error(`Error: ${err}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getReport()
+  }, [])
+
+  const handleClickNewOrder = () => {
+    router.push(`/orders/new`);
+  }
+
+  return <>
+    <div className="flex justify-between gap-10">
+      <HeaderTitle
+        title="Report"
+        label="These are your sales reports"
+      />
+
+      <div className="mb-14 flex justify-end">
+        <Button
+          label="New Order"
+          variant="primary"
+          onClick={(handleClickNewOrder)}
+        />
+      </div>
+    </div>
+
+    <div className="grid gap-5">
+      <div className="h-fit col-span-2 grid grid-cols-3 gap-x-3 gap-y-5">
+        <Card
+          title="Orders"
+          label="Total in orders"
+          value={report.orders}
+        />
+        <Card
+          title="Sales"
+          label="Total in sales"
+          value={report.totalSales}
+          type="money"
+        />
+        <Card
+          title="Accumulated"
+          label="Total in sales"
+          value={report.allSales}
+          type="money"
+        />
+
+        <div className="col-span-3">
+          <ChartAllSales />
+        </div>
+      </div>
+    </div>
+
+    {
+      isLoading &&
+      <LoadingDefault />
+    }
+  </>
 }
---
+++ src/components/ChartAllSales.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { api } from '@/lib/api';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Chart } from 'react-google-charts';
+import { toast } from 'react-toastify';
+
+const data = [
+  ["Month", "Total Sales"],
+  ["January", 1000],
+  ["February", 1200],
+  ["March", 800],
+  ["April", 1500],
+  ["May", 2000],
+  ["June", 1800],
+  ["July", 2200],
+  ["August", 2500],
+  ["September", 2100],
+  ["October", 1900],
+  ["November", 2300],
+  ["December", 2800],
+];
+
+const options = {
+  bars: "vertical",
+};
+
+interface AllSalesProps { }
+
+export default function ChartAllSales() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [sales, setSales] = useState<AllSalesProps[]>([["Month", "Total Sales"], ['not found', 0]])
+
+  const router = useRouter();
+
+  async function getMetricsSales() {
+    setIsLoading(true);
+    const token = Cookies.get('token')
+
+    try {
+      const allSales = await api.get('/year-sales-metrics', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      if (allSales.data.allSales) {
+        setSales([
+          ["Month", "Total Sales"],
+          ...allSales.data.allSales,
+        ])
+      }
+
+    } catch (err) {
+      // @ts-ignore
+      if (err.response.status === 401) {
+        Cookies.remove('token');
+        router.push('/login');
+      }
+      toast.error(`Error: ${err}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getMetricsSales();
+  }, [])
+
+  useEffect(() => {
+    console.log(sales)
+  }, [sales])
+
+  return <>
+    {
+      isLoading ?
+        <div className='min-h-[350px] w-full flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100'>
+          loading...
+        </div>
+        :
+        <Chart
+          chartType="Line"
+          width="100%"
+          height="auto"
+          className="min-h-[350px] bg-white rounded-xl p-2 overflow-hidden border-2 border-zinc-200 dark:border-zinc-700"
+          data={sales}
+          options={options}
+        />
+    }
+  </>
+}
